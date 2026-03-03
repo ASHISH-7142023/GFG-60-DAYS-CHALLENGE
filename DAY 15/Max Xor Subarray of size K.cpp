691ec4946888407107321c84ccb0ee325beb7200
ASHISH-7142023 <singhashishranjan362@gmail.com>
2026-03-04 00:36:42 +0530

    GFG 60 DAYS CHALLENGE

---
+++ DAY 15/Max Xor Subarray of size K.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    int maxSubarrayXOR(vector<int> &arr, int k)
+    {
+        int n = arr.size();
+        if (k > n)
+            return 0;
+
+        int currentXor = 0;
+        for (int i = 0; i < k; i++)
+        {
+            currentXor ^= arr[i];
+        }
+        int maxXor = currentXor;
+        for (int i = k; i < n; i++)
+        {
+            currentXor = currentXor ^ arr[i - k] ^ arr[i];
+            if (currentXor > maxXor)
+            {
+                maxXor = currentXor;
+            }
+        }
+        return maxXor;
+    }
+};
