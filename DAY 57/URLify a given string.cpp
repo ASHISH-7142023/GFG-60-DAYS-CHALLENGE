13602acef0e8973edbeebfa3b2d8a209d978bee7
ASHISH-7142023 <singhashishranjan362@gmail.com>
2026-04-15 15:54:07 +0530

    GFG 60 DAYS CHALLENGE

---
+++ DAY 57/URLify a given string.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    string URLify(string &s)
+    {
+        string res = "";
+
+        for (char c : s)
+        {
+            if (c == ' ')
+            {
+                res += "%20";
+            }
+            else
+            {
+                res += c;
+            }
+        }
+
+        return res;
+    }
+};
